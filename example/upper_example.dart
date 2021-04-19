4f2543566d0bca3be15497d9577a4eb3bc95e95a
andriwsluna <47540616+andriwsluna@users.noreply.github.com>
2021-04-18 23:29:05 -0300

    first commit

---
+++ example/upper_example.dart
@@ -0,0 +1,6 @@
+import 'package:upper/upper.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/upper.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library upper;
+
+export 'src/upper_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
