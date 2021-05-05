f66c6241bea3341979e706fce1428eb84ad2915f
andriwsluna <47540616+andriwsluna@users.noreply.github.com>
2021-05-04 23:00:01 -0300

    making executables

---
+++ bin/create.dart
@@ -0,0 +1,7 @@
+// import 'package:upper/static_grpc/project_creator.dart';
+// import 'package:postgres/postgres.dart';
+// import 'dart:io';
+
+Future<void> main(List<String> args) async {
+  print(args);
+}
