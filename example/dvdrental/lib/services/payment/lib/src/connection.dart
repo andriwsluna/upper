1b59e203e87c7f1e1e5cbced0c7eaecc2fd54e01
andriwsluna <47540616+andriwsluna@users.noreply.github.com>
2021-05-14 21:58:50 -0300

    example update

--- example/dvdrental/lib/services/payment/lib/src/connection.dart
+++ example/dvdrental/lib/services/payment/lib/src/connection.dart
@@ -1,6 +1,10 @@
 import 'package:postgres/postgres.dart';
 
-var _pgConnection = PostgreSQLConnection('192.168.1.81', 5432, 'dvdrental',
-    username: 'postgres', password: '1489', useSSL: false);
+var _pgConnection = PostgreSQLConnection(
+    'ec2-3-224-251-47.compute-1.amazonaws.com', 5432, 'db5pbo0gcf628i',
+    username: 'lprklljaicvzti',
+    useSSL: true,
+    password:
+        '1614cea156aa98660ad86987bd6b2fc5d5fab3167a2f035e6b43f8c1024238e4');
 
 PostgreSQLConnection getConnection() => _pgConnection;
