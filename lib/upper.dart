4a8b33eded8b6b1430b26d25b5d0749905fbb32e
andriwsluna <47540616+andriwsluna@users.noreply.github.com>
2021-05-17 17:00:50 -0300

    unexpose comand tools

--- lib/upper.dart
+++ lib/upper.dart
@@ -2,7 +2,6 @@
 /// Make a automated GRPC API For Relational Databases.
 library upper;
 
-export 'src/static_grpc_framework.dart';
 export 'package:data_db/data_db.dart';
 export 'package:static_postgres_orm/static_postgres_orm.dart';
 export 'package:static_grpc/static_grpc.dart';
